45519271157ca590e336e2fd8b5582508fa9e188
Antonio Guerrero <a.guerrero.esp94@gmail.com>
2020-08-11 16:27:16 -0500

    clean code, use a new RecoveridModel

--- recoverid/cities/models/city.py
+++ recoverid/cities/models/city.py
@@ -4,14 +4,15 @@ from django.db import models
 from recoverid.utils.models import RecoveridModel
 from recoverid.states.models import State
 
+
 class City(RecoveridModel):
     """City Model"""
     city_id = models.AutoField(primary_key=True)
-    city_name = models.CharField(max_length=50, blank = True)
+    cityt_name = models.CharField(max_length=50,null = False,blank = False)
     state_id = models.ForeignKey(State,on_delete=models.CASCADE) 
-    flag  =  models.CharField(max_length=500)
-    population = models.BigIntegerField()
-
+    
+    
     def __str__(self):
         """ return city name """
         return self.city_name
+
